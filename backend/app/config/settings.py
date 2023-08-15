c0b209155e939698ba9debb3b4bbc237622d89a5
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-08-15 23:01:42 +0530

    Delete backend directory

--- backend/app/config/settings.py
+++
@@ -1,2 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///library.db'  # Change this to your desired database URI
-SQLALCHEMY_TRACK_MODIFICATIONS = False
--- backend/app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-books_bp = Blueprint('books', __name__)
-members_bp = Blueprint('members', __name__)
-
-from app.routes import books, members  # Import your route files
